d6f1bcb04aba102e5b588638ce2fa8312b0eed2e
OustanDing <oustan10@gmail.com>
2018-11-11 15:06:31 -0500

    most common bigrams that do not include stopwords

--- bigrams.py
+++ bigrams.py
@@ -9,14 +9,15 @@ f = open('orwellEssays.txt', 'r')
 if f.mode == 'r':
     contents = f.read()
 
-# TOP 100 WORDS
+# TOP 100 BIGRAMS
 words = tokenize.tokenize(contents)
+bigrams = nltk.bigrams(words)
 
-filteredWords = [word for word in words if not word in common]
+filteredBigrams = [bigram for bigram in bigrams if bigram[0] not in common and bigram[1] not in common]
 
-fdist = nltk.FreqDist(filteredWords)
+fdist = nltk.FreqDist(filteredBigrams)
 
-w = open('top100.txt', 'w')
+w = open('bigrams.txt', 'w+')
 
 for entry in fdist.most_common(100):
-    w.write(entry[0] + '\n')
+    w.write(entry[0][0] + ' ' + entry[0][1] + '\n')
